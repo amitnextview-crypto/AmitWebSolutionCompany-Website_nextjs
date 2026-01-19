22524c49ad86fca2af61450028e028d17de13d78
Amit <amitnextview@gmail.com>
2026-01-19 19:47:10 +0530

    fix err for meta tag for google consoleagain

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://amit-web-solution-company-website-n.vercel.app',
   generateRobotsTxt: true,
+  exclude: ['/icon.png'], // exclude unnecessary files
+  changefreq: 'daily',
+  priority: 0.8,
 };
