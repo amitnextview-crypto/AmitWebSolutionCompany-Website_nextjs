ccbf95c2d361dba5cda31b1d5b208526bdf8677a
Amit <amitnextview@gmail.com>
2026-01-19 19:22:31 +0530

    set meta tag for google console

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,3 @@
-
 import './globals.css';
 
 export const metadata = {
@@ -10,6 +9,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="mBt9pJk7MgShnY53Su_xOzZQXQBuunHWQ_VgWeEs_ZE"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
